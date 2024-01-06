df9e6f600cc61b5369fa0ed59a8b73cf0829502a
Shinichi Nomura <shinichi.nomura@gmail.com>
2024-01-06 22:14:27 +0900

    レシピページのコンテンツを取得できないケースに対応

--- app/controllers/secure/recipes_controller.rb
+++ app/controllers/secure/recipes_controller.rb
@@ -13,7 +13,7 @@ class Secure::RecipesController < Secure::ApplicationController
           id: recipe.id,
           title: recipe.title,
           url: recipe.url,
-          image_url: url_for(recipe.image),
+          image_url: recipe.image.present? ? url_for(recipe.image) : nil,
           registered_at: recipe.registered_at
         }
       end
@@ -26,32 +26,47 @@ class Secure::RecipesController < Secure::ApplicationController
     og_title = nil
     og_image_url = nil
 
-    uri = URI.open(escape_url_ja(url))
+    begin
+      uri = URI.open(
+        escape_url_ja(url),
+        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
+      )
 
-    html = Nokogiri::HTML.parse(uri.read)
+      html = Nokogiri::HTML.parse(uri.read)
 
-    og_title_elem = html.search('meta[property="og:title"]').first
-    if og_title_elem.present?
-      og_title = og_title_elem.attr(:content) || ''
-    end
-
-    og_image_elem = html.search('meta[property="og:image"]').first
-
-    if og_image_elem.present?
-      og_image_url = og_image_elem.attr(:content)
-
-      if og_image_url[0] === '/'
-        parsed_uri = URI.parse(escape_url_ja(url))
-        parsed_uri.path = og_image_url
-        og_image_url = parsed_uri
+      og_title_elem = html.search('meta[property="og:title"]').first
+      if og_title_elem.present?
+        og_title = og_title_elem.attr(:content) || ''
       end
-    end
 
-    render json: {
-      statue: "success",
-      og_title: og_title,
-      og_image_url: og_image_url
-    }
+      og_image_elem = html.search('meta[property="og:image"]').first
+
+      if og_image_elem.present?
+        og_image_url = og_image_elem.attr(:content)
+
+        if og_image_url[0] === '/'
+          parsed_uri = URI.parse(escape_url_ja(url))
+          parsed_uri.path = og_image_url
+          og_image_url = parsed_uri
+        end
+      end
+
+      if og_title.blank?
+        render json: {
+          status: "error"
+        }
+      else
+        render json: {
+          status: "success",
+          og_title: og_title,
+          og_image_url: og_image_url
+        }
+      end
+    rescue => e
+      render json: {
+        statue: "error"
+      }
+    end
   end
 
   def create
@@ -62,11 +77,13 @@ class Secure::RecipesController < Secure::ApplicationController
       registered_at: Time.current
     )
 
-    image_url = params[:image_url]
-    image_io = URI.open(escape_url_ja(image_url))
-    image_filename = "image.#{image_io.content_type.split('/').last}"
+    if params[:image_url].present?
+      image_url = params[:image_url]
+      image_io = URI.open(escape_url_ja(image_url))
+      image_filename = "image.#{image_io.content_type.split('/').last}"
 
-    recipe.image.attach(io: image_io, filename: image_filename, content_type: image_io.content_type)
+      recipe.image.attach(io: image_io, filename: image_filename, content_type: image_io.content_type)
+    end
 
     if recipe.save
       render json: {
