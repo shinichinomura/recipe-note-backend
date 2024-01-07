5bdc1a10904b3b9c56ee8899a9f2a908d0b15b63
Shinichi Nomura <shinichi.nomura@gmail.com>
2024-01-08 01:00:35 +0900

    環境別CORS設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.cors_allow_origins = ["https://recipe-note.me"]
 end
